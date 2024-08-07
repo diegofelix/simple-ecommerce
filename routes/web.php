a753a9f6dd0050572bcb96ca74d7c90be51b45fa
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-07 20:23:23 -0300

    feat: add products page html

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('categories', CategoriesPage::class);
+Route::get('products', ProductsPage::class);
