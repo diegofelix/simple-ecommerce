c80074744da1994bfa5ee1a54b66021a6aaa3168
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-07 20:26:58 -0300

    feat: add product details page html

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('categories', CategoriesPage::class);
 Route::get('products', ProductsPage::class);
+Route::get('products/{product}', ProductDetailPage::class);
 Route::get('cart', CartPage::class);
