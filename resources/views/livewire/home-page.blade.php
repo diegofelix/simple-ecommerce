3398c60e8c815903ca0daff0405bef48a70cb692
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-07 10:36:03 -0300

    feat: using frontend in a separeted container

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-red-500">Diego Felix</h1>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,56 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        // https: true,
+        hmr: {
+            host: 'frontend.local.diegofelix.com.br',
+            protocol: 'wss',
+            // clientPort: 443,
+        },
+    },
+
+
+    /*
+    // dessa forma aqui da badgaway no browser e no laravel
+    // o laravel também reclama de CORS
+    server: {
+        https: true,
+        hmr: {
+            host: 'frontend.local.diegofelix.com.br',
+            protocol: 'wss',
+            clientPort: 443,
+        },
+    },
+     */
+
+    /*
+    // dessa forma aqui da badgateway no browser
+    // se rodar frontend.local.diegofelix.com.br:5173 ou acessando laravel
+    // da erro de ssl ERR_SSL_VERSION_OR_CIPHER_MISMATCH
+    server: {
+        https: true,
+        hmr: {
+            host: 'frontend.local.diegofelix.com.br',
+            protocol: 'wss',
+            // clientPort: 443,
+        },
+    },
+    */
+
+    /*
+    // dessa forma aqui meio que funciona, porque o script eh carregado via https
+    // porque o traefik faz esse trabalho de transformar em ssl
+    // porem o @vite que tem no laravel usa http em vez de https.
+    // ou seja, funciona no navegador bonitinho, mas o front do laravel tenta
+    // carregar http em vez de https
+    server: {
+        // https: true,
+        hmr: {
+            host: 'frontend.local.diegofelix.com.br',
+            // protocol: 'wss',
+            // clientPort: 443,
+        },
+    },
+    */
 });
