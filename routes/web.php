2883aa52a0465849a6910a502358f443f5d71ce0
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-07 20:21:56 -0300

    feat: add categories page html

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('categories', CategoriesPage::class);
