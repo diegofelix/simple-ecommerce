b9d1810e8e08c31d0e947801a30b5e93efbb1946
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-08 08:06:43 -0300

    feat: make home page show categories and brands from database

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Home Page - Ecommerce')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', true)->get();
+        $categories = Category::where('is_active', true)->get();
+
+        return view('livewire.home-page', compact('brands', 'categories'));
     }
 }
