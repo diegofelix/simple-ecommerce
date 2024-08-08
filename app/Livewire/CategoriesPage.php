f14d17110899ca9dedcca3f51367f89746fd947a
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-08 13:35:00 -0300

    feat: make category page show data from database

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
+#[Title('Categories - Ecommerce')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where(['is_active' => true])->get();
+
+        return view('livewire.categories-page', compact('categories'));
     }
 }
