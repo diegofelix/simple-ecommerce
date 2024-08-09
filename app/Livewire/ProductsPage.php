b50479e615293628f220a2d08596612b990b0f02
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-08 22:14:28 -0300

    feat: make products page get data from database

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products - Ecommerce')]
 class ProductsPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.products-page');
+        $products = Product::query()
+            ->where('is_active', true)
+            ->paginate(6);
+
+        $brands = Brand::where('is_active', true)->get(['id', 'name', 'slug']);
+        $categories = Category::where('is_active', true)->get(['id', 'name', 'slug']);
+
+        return view(
+            'livewire.products-page',
+            compact('products', 'brands', 'categories')
+        );
     }
 }
