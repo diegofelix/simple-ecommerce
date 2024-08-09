3548e93e1efdf522eb9e6f923dbabebc962b66b6
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-08 21:19:19 -0300

    fix(preline) make hamburger menu runs every wire:navigate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,11 @@
 import './bootstrap';
 import 'preline';
+
+// When using livewire, we need to reinitialize the scripts after each navigation
+// This is because the scripts are not reloaded when using Turbolinks
+// @see https://livewire.laravel.com/docs/navigate#dont-rely-on-domcontentloaded
+document.addEventListener('livewire:navigated', () => {
+    // This code below reinitialize the scripts from preline package
+    // (The hamburger menu and the dropdowns)
+    window.HSStaticMethods.autoInit();
+})
