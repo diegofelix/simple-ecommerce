3698fd9628ffb958c3f64e5e3093695e97f9f1fe
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-10 20:15:27 -0300

    feat: make cart page work

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Cart - Ecommerce')]
 class CartPage extends Component
 {
+    public array $cartItems = [];
+    public int $totalPrice;
+
+    public function mount(): void
+    {
+        $this->cartItems = CartManagement::getCartItems();
+        $this->totalPrice = CartManagement::calculateTotalPrice($this->cartItems);
+    }
+
+    public function removeFromCart(int $productId): void
+    {
+        $this->cartItems = CartManagement::removeCartItem($productId);
+        $this->totalPrice = CartManagement::calculateTotalPrice($this->cartItems);
+        $this->dispatch('cartUpdatedCount', totalCount: count($this->cartItems))->to(Navbar::class);
+    }
+
+    public function increaseQuantity(int $productId): void
+    {
+        $this->cartItems = CartManagement::incrementQuantityToCartItem($productId);
+        $this->totalPrice = CartManagement::calculateTotalPrice($this->cartItems);
+    }
+
+    public function decreaseQuantity(int $productId): void
+    {
+        $this->cartItems = CartManagement::decrementQuantityToCartItem($productId);
+        $this->totalPrice = CartManagement::calculateTotalPrice($this->cartItems);
+    }
+
     public function render()
     {
         return view('livewire.cart-page');
--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -1,3 +1,4 @@
+@use(Illuminate\Support\Number)
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
     <div class="container mx-auto px-4">
         <h1 class="text-2xl font-semibold mb-4">Shopping Cart</h1>
@@ -15,25 +16,38 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td class="py-4">
-                                <div class="flex items-center">
-                                    <img class="h-16 w-16 mr-4" src="https://via.placeholder.com/150" alt="Product image">
-                                    <span class="font-semibold">Product name</span>
-                                </div>
-                            </td>
-                            <td class="py-4">$19.99</td>
-                            <td class="py-4">
-                                <div class="flex items-center">
-                                    <button class="border rounded-md py-2 px-4 mr-2">-</button>
-                                    <span class="text-center w-8">1</span>
-                                    <button class="border rounded-md py-2 px-4 ml-2">+</button>
-                                </div>
-                            </td>
-                            <td class="py-4">$19.99</td>
-                            <td><button class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">Remove</button></td>
-                        </tr>
-                        <!-- More product rows -->
+                        @forelse($cartItems as $cartItem)
+                            <tr wire:key="{{ $cartItem['product_id'] }}">
+                                <td class="py-4">
+                                    <div class="flex items-center">
+                                        <img class="h-16 w-16 mr-4" src="{{ url('storage', $cartItem['image']) }}"
+                                             alt="Product image">
+                                        <span class="font-semibold">{{ $cartItem['name'] }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-4">{{ Number::currency($cartItem['unit_price'], 'BRL') }}</td>
+                                <td class="py-4">
+                                    <div class="flex items-center">
+                                        <button wire:click="decreaseQuantity({{ $cartItem['product_id'] }})" class="border rounded-md py-2 px-4 mr-2" @if($cartItem['quantity'] <= 1) disabled @endif>-</button>
+                                        <span class="text-center w-8">{{ $cartItem['quantity'] }}</span>
+                                        <button wire:click="increaseQuantity({{ $cartItem['product_id'] }})" class="border rounded-md py-2 px-4 ml-2">+</button>
+                                    </div>
+                                </td>
+                                <td class="py-4">{{ Number::currency($cartItem['total_price'], 'BRL') }}</td>
+                                <td>
+                                    <button
+                                        wire:click="removeFromCart({{ $cartItem['product_id'] }})"
+                                        class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
+                                        <span wire:loading.remove wire:target="removeFromCart({{ $cartItem['product_id'] }})">Remove</span>
+                                        <span wire:loading wire:target="removeFromCart({{ $cartItem['product_id'] }})">Removing</span>
+                                    </button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="text-center py-4 text-4xl text-semibold text-slate-500">No items in cart</td>
+                            </tr>
+                        @endforelse
                         </tbody>
                     </table>
                 </div>
@@ -43,22 +57,24 @@
                     <h2 class="text-lg font-semibold mb-4">Summary</h2>
                     <div class="flex justify-between mb-2">
                         <span>Subtotal</span>
-                        <span>$19.99</span>
+                        <span>{{ Number::currency($totalPrice, 'BRL') }}</span>
                     </div>
                     <div class="flex justify-between mb-2">
                         <span>Taxes</span>
-                        <span>$1.99</span>
+                        <span>{{ Number::currency(0, 'BRL') }}</span>
                     </div>
                     <div class="flex justify-between mb-2">
                         <span>Shipping</span>
-                        <span>$0.00</span>
+                        <span>{{ Number::currency(0, 'BRL') }}</span>
                     </div>
                     <hr class="my-2">
                     <div class="flex justify-between mb-2">
                         <span class="font-semibold">Total</span>
-                        <span class="font-semibold">$21.98</span>
+                        <span class="font-semibold">{{ Number::currency($totalPrice, 'BRL') }}</span>
                     </div>
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                    @if ($cartItems)
+                        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                    @endif
                 </div>
             </div>
         </div>
