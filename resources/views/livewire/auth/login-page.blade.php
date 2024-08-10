21f69903458b892ebf3f15b521fe5a83314faceb
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-10 20:51:44 -0300

    fix: add error when user use wrong password

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,6 +17,11 @@
 
                     <!-- Form -->
                     <form wire:submit.prevent="save">
+                        @if(session('error'))
+                            <div class="mt-2 bg-red-500 text-sm text-white rounded-lg p-4 mb-4" role="alert" tabindex="-1" aria-labelledby="hs-solid-color-danger-label">
+                                <span id="hs-solid-color-danger-label" class="font-bold">Ops!</span> {{ session('error') }}
+                            </div>
+                        @endif
                         <div class="grid gap-y-4">
                             <!-- Form Group -->
                             <div>
