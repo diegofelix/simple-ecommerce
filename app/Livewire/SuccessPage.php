3d71fd0230ef5e757793d0570b466c1ba90222d8
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-11 18:52:22 -0300

    feat: add data to order success page

--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Success - Ecommerce')]
 class SuccessPage extends Component
 {
     public function render()
     {
-        return view('livewire.success-page');
+        $order = Order::with('address')
+            ->where('user_id', auth()->id())
+            ->latest()
+            ->first();
+
+        return view('livewire.success-page', compact('order'));
     }
 }
