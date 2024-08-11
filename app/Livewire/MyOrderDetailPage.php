86b7390f5891be58773fc72f7ed52691ed849690
Diego Felix <diegoflx.oliveira@gmail.com>
2024-08-11 19:53:24 -0300

    feat: make my order detail page dynamic

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('My Order Detail - Ecommerce')]
 class MyOrderDetailPage extends Component
 {
+    public Order $order;
+
+    public function mount(Order $order)
+    {
+        $this->order = $order;
+    }
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        return view('livewire.my-order-detail-page', [
+            'order' => $this->order->load('items.product', 'address'),
+        ]);
     }
 }
--- resources/views/livewire/my-order-detail-page.blade.php
+++ resources/views/livewire/my-order-detail-page.blade.php
@@ -1,3 +1,30 @@
+@use(Illuminate\Support\Number)
+@php
+    $statusColor = 'bg-orange-500';
+    $paymentStatusColor = 'bg-red-500';
+    if ($order->status == 'new') {
+        $statusColor = 'bg-blue-500';
+    }
+    if ($order->status == 'processing') {
+        $statusColor = 'bg-yellow-500';
+    }
+    if ($order->status == 'shipped' || $order->status == 'delivered') {
+        $statusColor = 'bg-green-500';
+    }
+    if ($order->status == 'canceled') {
+        $statusColor = 'bg-red-500';
+    }
+
+    if ($order->payment_status == 'paid') {
+        $paymentStatusColor = 'bg-green-500';
+    }
+    if ($order->payment_status == 'pending') {
+        $paymentStatusColor = 'bg-yellow-500';
+    }
+    if ($order->payment_status == 'canceled') {
+        $paymentStatusColor = 'bg-red-500';
+    }
+@endphp
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
     <h1 class="text-4xl font-bold text-slate-500">Order Details</h1>
 
@@ -6,12 +33,15 @@
         <!-- Card -->
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
             <div class="p-4 md:p-5 flex gap-x-4">
-                <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
-                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                        <circle cx="9" cy="7" r="4" />
-                        <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                <div
+                    class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
+                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400"
+                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/>
+                        <circle cx="9" cy="7" r="4"/>
+                        <path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
+                        <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
                     </svg>
                 </div>
 
@@ -22,7 +52,7 @@
                         </p>
                     </div>
                     <div class="mt-1 flex items-center gap-x-2">
-                        <div>Jace Grimes</div>
+                        <div>{{ $order->address->full_name }}</div>
                     </div>
                 </div>
             </div>
@@ -32,12 +62,15 @@
         <!-- Card -->
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
             <div class="p-4 md:p-5 flex gap-x-4">
-                <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
-                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M5 22h14" />
-                        <path d="M5 2h14" />
-                        <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22" />
-                        <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
+                <div
+                    class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
+                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400"
+                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 22h14"/>
+                        <path d="M5 2h14"/>
+                        <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22"/>
+                        <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2"/>
                     </svg>
                 </div>
 
@@ -49,7 +82,7 @@
                     </div>
                     <div class="mt-1 flex items-center gap-x-2">
                         <h3 class="text-xl font-medium text-gray-800 dark:text-gray-200">
-                            17-02-2024
+                            {{ $order->created_at->format('Y-m-d H:i') }}
                         </h3>
                     </div>
                 </div>
@@ -60,10 +93,13 @@
         <!-- Card -->
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
             <div class="p-4 md:p-5 flex gap-x-4">
-                <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
-                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" />
-                        <path d="m12 12 4 10 1.7-4.3L22 16Z" />
+                <div
+                    class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
+                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400"
+                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6"/>
+                        <path d="m12 12 4 10 1.7-4.3L22 16Z"/>
                     </svg>
                 </div>
 
@@ -74,7 +110,7 @@
                         </p>
                     </div>
                     <div class="mt-1 flex items-center gap-x-2">
-                        <span class="bg-yellow-500 py-1 px-3 rounded text-white shadow">Processing</span>
+                        <span class="{{ $statusColor }} py-1 px-3 rounded text-white shadow">{{ $order->status }}</span>
                     </div>
                 </div>
             </div>
@@ -84,12 +120,15 @@
         <!-- Card -->
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
             <div class="p-4 md:p-5 flex gap-x-4">
-                <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
-                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M5 12s2.545-5 7-5c4.454 0 7 5 7 5s-2.546 5-7 5c-4.455 0-7-5-7-5z" />
-                        <path d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                        <path d="M21 17v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2" />
-                        <path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2" />
+                <div
+                    class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
+                    <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400"
+                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12s2.545-5 7-5c4.454 0 7 5 7 5s-2.546 5-7 5c-4.455 0-7-5-7-5z"/>
+                        <path d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                        <path d="M21 17v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2"/>
+                        <path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2"/>
                     </svg>
                 </div>
 
@@ -100,7 +139,8 @@
                         </p>
                     </div>
                     <div class="mt-1 flex items-center gap-x-2">
-                        <span class="bg-green-500 py-1 px-3 rounded text-white shadow">Paid</span>
+                        <span
+                            class="{{ $paymentStatusColor }} py-1 px-3 rounded text-white shadow">{{ $order->payment_status }}</span>
                     </div>
                 </div>
             </div>
@@ -123,35 +163,23 @@
                     </thead>
                     <tbody>
 
-                    <!--[if BLOCK]><![endif]-->
-                    <tr wire:key="53">
-                        <td class="py-4">
-                            <div class="flex items-center">
-                                <img class="h-16 w-16 mr-4" src="http://localhost:8000/storage/products/01HND3J5XS7ZC5J84BK5YDM6Z2.jpg" alt="Product image">
-                                <span class="font-semibold">Samsung Galaxy Watch6</span>
-                            </div>
-                        </td>
-                        <td class="py-4">₹29,999.00</td>
-                        <td class="py-4">
-                            <span class="text-center w-8">1</span>
-                        </td>
-                        <td class="py-4">₹29,999.00</td>
-                    </tr>
-                    <tr wire:key="54">
-                        <td class="py-4">
-                            <div class="flex items-center">
-                                <img class="h-16 w-16 mr-4" src="http://localhost:8000/storage/products/01HND30J0P7C6MWQ1XQK7YDQKA.jpg" alt="Product image">
-                                <span class="font-semibold">Samsung Galaxy Book3</span>
-                            </div>
-                        </td>
-                        <td class="py-4">₹75,000.00</td>
-                        <td class="py-4">
-                            <span class="text-center w-8">5</span>
-                        </td>
-                        <td class="py-4">₹375,000.00</td>
-                    </tr>
-                    <!--[if ENDBLOCK]><![endif]-->
-
+                        <!--[if BLOCK]><![endif]-->
+                        @foreach ($order->items as $item)
+                            <tr wire:key="{{ $item->product_id }}">
+                                <td class="py-4">
+                                    <div class="flex items-center">
+                                        <img class="h-16 w-16 mr-4" src="{{ url('storage', $item->product->images[0]) }}"
+                                             alt="Product image">
+                                        <span class="font-semibold">{{ $item->product->name }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-4">{{ Number::currency($item->unit_price, 'BRL') }}</td>
+                                <td class="py-4">
+                                    <span class="text-center w-8">1</span>
+                                </td>
+                                <td class="py-4">{{ Number::currency($item->total_price, 'BRL') }}</td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
@@ -160,11 +188,11 @@
                 <h1 class="font-3xl font-bold text-slate-500 mb-3">Shipping Address</h1>
                 <div class="flex justify-between items-center">
                     <div>
-                        <p>42227 Zoila Glens, Oshkosh, Michigan, 55928</p>
+                        <p>{{ $order->address->street_address }}, {{ $order->address->city }}, {{ $order->address->state }}, {{ $order->address->zip_code }}</p>
                     </div>
                     <div>
                         <p class="font-semibold">Phone:</p>
-                        <p>023-509-0009</p>
+                        <p>{{ $order->address->phone }}</p>
                     </div>
                 </div>
             </div>
@@ -175,20 +203,20 @@
                 <h2 class="text-lg font-semibold mb-4">Summary</h2>
                 <div class="flex justify-between mb-2">
                     <span>Subtotal</span>
-                    <span>₹404,999.00</span>
+                    <span>{{ Number::currency($order->total_purchase, 'BRL') }}</span>
                 </div>
                 <div class="flex justify-between mb-2">
                     <span>Taxes</span>
-                    <span>₹0.00</span>
+                    <span>{{ Number::currency(0, 'BRL') }}</span>
                 </div>
                 <div class="flex justify-between mb-2">
                     <span>Shipping</span>
-                    <span>₹0.00</span>
+                    <span>{{ Number::currency(0, 'BRL') }}</span>
                 </div>
                 <hr class="my-2">
                 <div class="flex justify-between mb-2">
                     <span class="font-semibold">Grand Total</span>
-                    <span class="font-semibold">₹404,999.00</span>
+                    <span class="font-semibold">{{ Number::currency($order->total_purchase, 'BRL') }}</span>
                 </div>
 
             </div>
